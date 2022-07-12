12c53a67453556863060901ee57f273bc273871e
Mavrik-jnr <mahmoud4u12@gmail.com>
2022-07-11 19:46:30 -0700

    Initialized with Firebase, added some color styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import Button from "./Components/Button.js";
 import "./Css/App.css";
+import { storageRef } from "./firebase.js";
 
 function App() {
+  // const img = new URL("murkets.appspot.com/image.png");
+  useEffect(() => {
+    // console.log(img);
+  }, []);
+
   return (
     <div className="App">
-      <Button />
+      {/* <Button /> */}
+      <div
+        style={{
+          backgroundImage: "https://murkets.appspot.com/image.png",
+        }}></div>
       <h1>Google</h1>
     </div>
   );
